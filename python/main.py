1299d29f25c21441c0aa3bbe6964ba3a8f3fdabe
Shahbaz Hussain <shahbaz.h96@gmail.com>
2018-05-08 23:32:15 +0530

    Added functionality for multiple results as well for handling error

--- python/main.py
+++ python/main.py
@@ -26,38 +26,56 @@ def obtain_result(named_entity, query_properties):
                             break
     print(property_code)
 
-    if (len(property_code) != 0):
-        sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-        query = """SELECT ?label ?property WHERE
-                    { 
-                    ?entity rdfs:label ?label .
-                    ?entity wdt:""" + property_code[0] + """ ?property_id .
-                    ?property_id rdfs:label ?property .
-                    FILTER (STR(?label) = '""" + named_entity[0] + """') .
-                    FILTER (LANG(?property) = "en")
-                    }
-                    LIMIT 1"""
-        print query
+    if (len(named_entity) != 0):
+        if (len(property_code) != 0):
+            sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+            query = """SELECT ?label ?property WHERE
+                        { 
+                        ?entity rdfs:label ?label .
+                        ?entity wdt:""" + property_code[0] + """ ?property_id .
+                        ?property_id rdfs:label ?property .
+                        FILTER (STR(?label) = '""" + named_entity[0] + """') .
+                        FILTER (LANG(?property) = "en")
+                        }"""
+            print query
+        else:
+            sparql = SPARQLWrapper("https://dbpedia.org/sparql")
+            query = """SELECT ?label ?description WHERE
+                        { 
+                        ?entity rdfs:label ?label .
+                        ?entity dbo:abstract ?description .
+                        FILTER (STR(?label) = '""" + named_entity[0] + """') .
+                        }"""
+            print query
+
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        result = []
+        try:
+            results = sparql.query().convert()
+            print(results)
+            for data in results["results"]["bindings"]:
+                result.append(data["property"]["value"])
+
+            result = list(set(result))
+            response = []
+            for i in result:
+                print(i)
+                response.append(str(i))
+            print(response)
+            data = {"status": "200", "data": response}
+            result = json.dumps(data)
+        except:
+            response = ["Unable to retrieve data"]
+            data = {"status": "500", "data": response}
+            result = json.dumps(data)
     else:
-        sparql = SPARQLWrapper("https://dbpedia.org/sparql")
-        query = """SELECT ?label ?description WHERE
-                    { 
-                    ?entity rdfs:label ?label .
-                    ?entity dbo:abstract ?description .
-                    FILTER (STR(?label) = '""" + named_entity[0] + """') .
-                    }"""
-        print query
+        response = ["Unable to retrieve data"]
+        data = {"status": "500", "data": response}
+        result = json.dumps(data)
 
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-    print(results)
-    result = str()
-    for result in results["results"]["bindings"]:
-        result = result["property"]["value"]
-    result = json.loads('{ "data" : "'+result+'"}')
+    print(result)
     return result
-
 
 
 app = Flask(__name__)
@@ -74,11 +92,11 @@ class Query(Resource):
         print query
         named_entity, query_properties = find_entities(query)
         result = obtain_result(named_entity, query_properties)
-        return jsonify(result)
+        return result
         # We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
 
 
-api.add_resource(Query, '/dept/<string:query>')
+api.add_resource(Query, '/wiki/<string:query>')
 api.add_resource(Departments_Meta, '/')
 
 if __name__ == '__main__':
